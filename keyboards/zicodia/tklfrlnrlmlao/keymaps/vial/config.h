cda8e5c14dc1fabec7dededa5016d2c35d420a2f
SeebM <sebastian.martens@mailbox.org>
2024-04-26 18:46:44 +0200

    enable vial support for zicodia/tklfrlnrlmlao

---
+++ keyboards/zicodia/tklfrlnrlmlao/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xA9, 0x43, 0x7D, 0x8B, 0xC8, 0xCA, 0xEE, 0x8D}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_TAP_DANCE_ENTRIES 20
